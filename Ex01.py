2a1083e8556625feb4e67cb001ae6c641c8b7573
watosnfan <30586391+watosnfan@users.noreply.github.com>
2021-03-04 21:26:29 +0800

    Add files via upload

---
+++ Ex01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 18:55:00 2021
+
+@author: user
+"""
+
+weight = 6
+
+if weight >= 100:
+    print("要減肥了")
+elif weight >= 80:
+    print("多運動")
+elif weight >= 60:
+    print("多吃點")
+else:
+    print("太瘦了")
+
+print("程式執行完畢")    
